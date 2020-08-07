f1498d017e929ce2750222c0c20c1ae59d991edb
anvit <anvit1@hotmail.com>
2020-08-07 15:34:17 -0700

    move fruit and welcome to components

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 
+Vue.component('welcome', {
+    props: ['name'],
+    template: '<p>Welcome {{ name }} </p>'
+});
+
+Vue.component('fruit', {
+    props: ['item'],
+    template: `
+        <li v-if="item.qty > 0" class="fruit">
+            <div class="fruit-name">
+                {{ item.emoji }}
+            </div>
+            <div class="fruit-footer">
+                <span v-bind:class="[ item.qty <= 2 ? 'lowStock' : 'highStock']" class="fruit-qty">
+                    Stock: {{ item.qty }}
+                </span>
+                <span class="fruit-button">
+                    <button v-on:click="$emit('add-item', item)">Add to Cart</button>
+                </span>
+            </div>
+        </li>`
+})
+
 const app = new Vue({
     el: '#app',
     data: {
